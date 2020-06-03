6eee6abb6685cd08f4cd9d86ae3642f403604e0f
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-06-03 04:24:39 -0300

    Login by the context API

--- src/context/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface AuthContextType {
-  name: string;
-}
-
-const AuthContext = createContext<AuthContextType>({} as AuthContextType);
-
-export default AuthContext;
