d3d382d5d01dcc87eb6ce98dae9af15cf5055c93
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-06-13 13:40:38 -0300

    Animation

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImage from '../../assets/sign-in-background.png';
@@ -10,20 +10,46 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
-export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImage}) no-repeat center;
+  background-size: cover;
+`;
 
-  place-content: center;
+export const Content = styled.div`
+  margin: 3.5% 0;
+  width: 340px;
+  text-align: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity:0;
+    transform: translateX(-50px);
+  }
+  to{
+    opacity:1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
-    margin: 80px 0;
+    margin: 60px 0;
     width: 340px;
     text-align: center;
+    justify-content: center;
 
     h1 {
       margin-bottom: 24px;
@@ -59,10 +85,4 @@ export const Content = styled.div`
       margin-right: 16px;
     }
   }
-`;
-
-export const Background = styled.div`
-  flex: 1;
-  background: url(${signInBackgroundImage}) no-repeat center;
-  background-size: cover;
 `;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackgroundImage from '../../assets/sign-up-background.png';
@@ -10,20 +10,48 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
-export const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUpBackgroundImage}) no-repeat center;
+  background-size: cover;
+`;
 
-  place-content: center;
+export const Content = styled.div`
+  margin: 3.5% 0;
+  width: 340px;
+  text-align: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity:0;
+    transform: translateX(70px);
+    width: 90%;
+  }
+  to{
+    opacity:1;
+    transform: translateX(0);
+    width: 100%;
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
-    margin: 80px 0;
+    margin: 60px 0;
     width: 340px;
     text-align: center;
+    justify-content: center;
 
     h1 {
       margin-bottom: 24px;
@@ -43,7 +71,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     /* margin-top: 24px; */
     text-decoration: none;
     transition: color 0.2s;
@@ -52,17 +80,11 @@ export const Content = styled.div`
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
 
     svg {
       margin-right: 16px;
     }
   }
-`;
-
-export const Background = styled.div`
-  flex: 1;
-  background: url(${signUpBackgroundImage}) no-repeat center;
-  background-size: cover;
 `;
